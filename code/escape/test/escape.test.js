e169fdded6ecdd17ba1b0fb3902ef47186ae3618
Azat Mardan <hi@azat.co>
2017-09-27 14:33:58 +0500

    add changes :sleepy:

--- code/escape/test/escape.test.js
+++ code/escape/test/escape.test.js
@@ -1,46 +1,46 @@
 const {expect} = require('chai'),
-    {escape, unescape} = require('../index.js')();
+    {escape, unescape} = require('../index.js')()
 
 describe('#escape', () => {
   it('converts & into &amp;', () => {
-    expect(escape('&')).equal('&amp;');
-  });
+    expect(escape('&')).equal('&amp;')
+  })
 
   it('converts " into &quot;', () => {
-    expect(escape('"')).equal('&quot;');
-  });
+    expect(escape('"')).equal('&quot;')
+  })
 
   it('converts \' into &#39;', () => {
-    expect(escape('\'')).equal('&#39;');
-  });
+    expect(escape('\'')).equal('&#39;')
+  })
 
   it('converts < into &lt;', () => {
-    expect(escape('<')).equal('&lt;');
-  });
+    expect(escape('<')).equal('&lt;')
+  })
 
   it('converts > into &gt;', () => {
-    expect(escape('>')).equal('&gt;');
-  });
-});
+    expect(escape('>')).equal('&gt;')
+  })
+})
 
 describe('#unescape', () => {
   it('converts &amp; into &', () => {
-    expect(unescape('&amp;')).equal('&');
-  });
+    expect(unescape('&amp;')).equal('&')
+  })
 
   it('converts &quot; into "', () => {
-    expect(unescape('&quot;')).equal('"');
-  });
+    expect(unescape('&quot;')).equal('"')
+  })
 
   it('converts &#39; into \'', () => {
-    expect(unescape('&#39;')).equal('\'');
-  });
+    expect(unescape('&#39;')).equal('\'')
+  })
 
   it('converts &lt; into <', () => {
-    expect(unescape('&lt;')).equal('<');
-  });
+    expect(unescape('&lt;')).equal('<')
+  })
 
   it('converts &gt; into >', () => {
-    expect(unescape('&gt;')).equal('>');
-  });
-});
+    expect(unescape('&gt;')).equal('>')
+  })
+})
