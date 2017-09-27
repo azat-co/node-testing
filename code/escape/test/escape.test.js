03c8af492fa436c93d849516bd0a65a1f47a0df7
Azat Mardan <hi@azat.co>
2017-09-27 14:23:31 +0500

    add changes :five:

--- code/escape/test/escape.test.js
+++ code/escape/test/escape.test.js
@@ -1,46 +1,46 @@
-var should = require('chai').should(),
+var expect = require('chai').expect(),
     {escape, unescape} = require('../index.js')();
 
 describe('#escape', function() {
   it('converts & into &amp;', function() {
-    escape('&').should.equal('&amp;');
+    expect(escape('&')).equal('&amp;');
   });
 
   it('converts " into &quot;', function() {
-    escape('"').should.equal('&quot;');
+    expect(escape('"')).equal('&quot;');
   });
 
   it('converts \' into &#39;', function() {
-    escape('\'').should.equal('&#39;');
+    expect(escape('\'')).equal('&#39;');
   });
 
   it('converts < into &lt;', function() {
-    escape('<').should.equal('&lt;');
+    expect(escape('<')).equal('&lt;');
   });
 
   it('converts > into &gt;', function() {
-    escape('>').should.equal('&gt;');
+    expect(escape('>')).equal('&gt;');
   });
 });
 
 describe('#unescape', function() {
   it('converts &amp; into &', function() {
-    unescape('&amp;').should.equal('&');
+    expect(unescape('&amp;')).equal('&');
   });
 
   it('converts &quot; into "', function() {
-    unescape('&quot;').should.equal('"');
+    expect(unescape('&quot;')).equal('"');
   });
 
   it('converts &#39; into \'', function() {
-    unescape('&#39;').should.equal('\'');
+    expect(unescape('&#39;')).equal('\'');
   });
 
   it('converts &lt; into <', function() {
-    unescape('&lt;').should.equal('<');
+    expect(unescape('&lt;')).equal('<');
   });
 
   it('converts &gt; into >', function() {
-    unescape('&gt;').should.equal('>');
+    expect(unescape('&gt;')).equal('>');
   });
 });
