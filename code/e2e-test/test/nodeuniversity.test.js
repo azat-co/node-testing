c76e9a5073ff80ac0e2a80d2d7ea46ad25830ce5
Azat Mardan <hi@azat.co>
2017-10-07 16:13:12 -0700

    add changes :heavy_plus_sign:

--- code/e2e-test/test/nodeuniversity.test.js
+++ code/e2e-test/test/nodeuniversity.test.js
@@ -1,38 +1,39 @@
+
 const webdriver = require('selenium-webdriver'),
   {By, until} = webdriver,
   {expect} = require('chai')
 
-  
-describe('azat.co', () => {
+
+describe('Node University', () => {
   var driver = new webdriver.Builder()
     .forBrowser('chrome')
     .build()
-  
+
   before(()=>{
-    driver.get('http://azat.co')  
+    driver.get('https://node.university')
   })
-  it('has list of books', async () => {
-    const h2 = await driver.findElement(By.xpath("//h2[contains(text(), '&nbsp;Books')]"))  
-    expect(h2).to.be.ok
+  it('has View All FREE Courses link', async () => {
+    const a = await driver.findElement(By.xpath("//a[contains(text(), 'View All FREE Courses')]"))
+    expect(a).to.be.ok
+    await a.click()
+    await driver.wait(until.titleIs('Node University: Courses on Node.js, React and JavaScript'), 1000)
+    expect(await driver.getCurrentUrl()).to.eql('https://node.university/courses/category/free')
+  })
+  it.only('has AWS course', async ()=>{
+    driver.wait(until.elementLocated(By.id('search-courses')))
+    driver.findElement(By.id('search-courses')).sendKeys('aws')
+    driver.findElement(By.id('search-courses')).sendKeys(webdriver.Key.ENTER) // same element as above just different selector
+    expect(await driver.getCurrentUrl()).to.eql('https://node.university/courses?query=aws')
+    const div = driver.findElement(By.xpath("//div[contains(text(), 'Node in Production Using Docker and AWS')]"))
+    expect(div).to.be.ok
+    div.click()
+    driver.wait(until.titleIs('Node in Production Using Docker and AWS | Node University: Courses on'), 1000)
+    expect(await driver.getCurrentUrl()).to.eql('https://node.university/p/node-in-production')
 
   })
-  // it('has AWS course', async ()=>{
-  //   driver.wait(until.elementLocated(By.id('search-courses')))
-  //   driver.findElement(By.id('search-courses')).sendKeys('aws') 
-  //   driver.findElement(By.id('search-courses')).sendKeys(webdriver.Key.ENTER) // same element as above just different selector
-  //   expect(await driver.getCurrentUrl()).to.eql('https://node.university/courses?query=aws')
-  //   const div = driver.findElement(By.xpath("//div[contains(text(), 'Node in Production Using Docker and AWS')]"))  
-  //   expect(div).to.be.ok
-  //   div.click()
-  //   driver.wait(until.titleIs('Node in | Node University: Courses on Node.js, React and JavaScript'), 1000)  
-  //   expect(await driver.getCurrentUrl()).to.eql('https://node.university/p/node-in-production')    
-    
-  // })
 
   after(() => {
     driver.sleep(20000)
     driver.quit()
   })
 })
-
-
